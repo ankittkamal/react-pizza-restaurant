35e786e332201ad5adbce9e27c4b8fe2530d8f8a
ankittkamal <officiallyankit999@gmail.com>
2023-08-10 17:13:02 +0530

    fetching order and form actions

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
   return (
     <header>
       <Link to="/">Pizza Resturant Co.</Link>
+      <SearchOrder />
       <p>Ankit</p>
     </header>
   );
